2f15a911c856a7713998c1f9e7714bae67c09f4f
Tymur <nicktimur228@gmail.com>
2024-05-17 16:05:00 +0300

    updated NetworkCalculator.swift

--- NetworkCalculator.swift
+++ NetworkCalculator.swift
@@ -13,44 +13,74 @@ class IPAddress {
     init?(string: String) {
         self.inner = (0,0,0,0)
         let result = self.withUInt8Pointer { pointer in
-            return string.withCString({ __IPAddressFromString(pointer, $0) })
+            string.withCString { __IPAddressFromString(pointer, $0) }
         }
         guard result != 0 else { return nil }
     }
     
+    init(copy other: IPAddress) {
+        self.inner = other.inner
+    }
+    
     var string: String {
-        return self.withUInt8Pointer { pointer in
-            let str = __stringIPAddress(pointer)!
-            defer { str.deallocate() }
-            return String(cString: str)
-        }
+        let str = __stringIPAddress(&self.inner.0)!
+        defer { str.deallocate() }
+        return String(cString: str)
     }
     
     var binary: String {
-        return self.withUInt8Pointer { pointer in
-            let str = __binaryIPAddress(pointer)!
-            defer { str.deallocate() }
-            return String(cString: str)
-        }
+        let str = __binaryIPAddress(&self.inner.0)!
+        defer { str.deallocate() }
+        return String(cString: str)
     }
     
     func withUInt8Pointer<Result>(_ body: (UnsafeMutablePointer<UInt8>) throws -> Result)
     rethrows -> Result
     {
-        return try body(&(self.inner.0))
+        return try body(&self.inner.0)
+    }
+    
+    subscript(offset: Int) -> UInt8 {
+        get { self.withUInt8Pointer { $0.advanced(by: offset).pointee } }
+        set { self.withUInt8Pointer { $0.advanced(by: offset).pointee = newValue } }
     }
 }
 
 
 class Network {
     private let inner: UnsafeMutablePointer<__Network>
+    lazy private(set) var address = {
+        let address = self.inner.pointee.address
+        return IPAddress(parts: [address.0, address.1, address.2, address.3])!
+    }()
+    lazy private(set) var mask = {
+        let mask = self.inner.pointee.mask
+        return IPAddress(parts: [mask.0, mask.1, mask.2, mask.3])!
+    }()
+    lazy private(set) var maskedBits = {
+        var masked: UInt8 = 0
+        for i in 0..<4 {
+            masked += (self.mask[i] >> 7) & 1
+            masked += (self.mask[i] >> 6) & 1
+            masked += (self.mask[i] >> 5) & 1
+            masked += (self.mask[i] >> 4) & 1
+            masked += (self.mask[i] >> 3) & 1
+            masked += (self.mask[i] >> 2) & 1
+            masked += (self.mask[i] >> 1) & 1
+            masked += (self.mask[i] >> 0) & 1
+            guard self.mask[i] == 0xff else { break }
+        }
+        return masked
+    }()
     
     deinit {
         inner.deallocate()
     }
     
     init?(address: String, mask: UInt8) {
-        guard let ntwk = address.withCString({ __CreateNetwork($0, mask) }) else { return nil }
+        guard let ntwk = address.withCString({ __CreateNetwork($0, mask) }) else {
+            return nil
+        }
         self.inner = ntwk
     }
     
@@ -65,21 +95,22 @@ class Network {
     }
     
     func subnetworks(hosts: [UInt32]) -> [Network]? {
-        var cp = hosts + [0]
-        guard let subnetworks = __CreateSubnetworks(self.inner, &cp) else { return nil }
+        var zeroed = hosts + [0]
+        guard let subnetworks = __CreateSubnetworks(self.inner, &zeroed) else { return nil }
         defer { subnetworks.deallocate() }
         return (0..<hosts.count).map { Network(inner: subnetworks.advanced(by: $0).pointee!) }
     }
     
     var firstAddress: IPAddress {
-        let result = IPAddress.zero
-        result.withUInt8Pointer { __firstIPAddress($0, self.inner) }
-        return result
+        let address = IPAddress.zero
+        address.withUInt8Pointer { __firstIPAddress($0, self.inner) }
+        return address
     }
     
     var lastAddress: IPAddress {
-        let result = IPAddress.zero
-        result.withUInt8Pointer { __lastIPAddress($0, self.inner) }
-        return result
+        let address = IPAddress.zero
+        address.withUInt8Pointer { __lastIPAddress($0, self.inner) }
+        return address
     }
 }
+
